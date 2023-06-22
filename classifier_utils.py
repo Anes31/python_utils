93afc252704a7bb6d867fc5343ee0cf2a34612bc
Anes <anes.wahran@gmail.com>
2023-06-22 14:24:12 -0500

    Initial commit

---
+++ classifier_utils.py
@@ -0,0 +1,266 @@
+def lightgbm_binary(trial, scoring, cv):
+    params = {
+        'objective': 'multiclass',
+        'boosting_type': 'gbdt',
+        'metric': 'multi_logloss',
+        'n_estimators': trial.suggest_int('n_estimators', 10, 500),
+        'max_depth': trial.suggest_int('max_depth', 1, 50),
+        'num_leaves': trial.suggest_int('num_leaves', 2, 100),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.1),
+        'min_child_samples': trial.suggest_int('min_child_samples', 1, 100),
+        'subsample': trial.suggest_uniform('subsample', 0.1, 1),
+        'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.1, 1),
+        'reg_alpha': trial.suggest_loguniform('reg_alpha', 1e-9, 10.0),
+        'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-9, 10.0),
+        'random_state': 0
+    }
+
+    model = LGBMClassifier(**params)
+
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def lightgbm_binary(trial, scoring, cv):
+    params = {
+        'objective': 'binary',
+        'boosting_type': 'gbdt',
+        'metric': 'binary_logloss',
+        'n_estimators': trial.suggest_int('n_estimators', 10, 500),
+        'max_depth': trial.suggest_int('max_depth', 1, 50),
+        'num_leaves': trial.suggest_int('num_leaves', 2, 100),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.1),
+        'min_child_samples': trial.suggest_int('min_child_samples', 1, 100),
+        'subsample': trial.suggest_uniform('subsample', 0.1, 1),
+        'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.1, 1),
+        'reg_alpha': trial.suggest_loguniform('reg_alpha', 1e-9, 10.0),
+        'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-9, 10.0),
+        'random_state': 0
+    }
+
+    model = LGBMClassifier(**params)
+
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def xgb(trial, scoring, cv):
+    params = {
+        'n_estimators': trial.suggest_int('n_estimators', 50, 1000),
+        'max_depth': trial.suggest_int('max_depth', 3, 10),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.1),
+        'subsample': trial.suggest_uniform('subsample', 0.1, 1),
+        'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.1, 1),
+        'reg_alpha': trial.suggest_loguniform('reg_alpha', 1e-10, 1),
+        'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-10, 1),
+        'gamma': trial.suggest_loguniform('gamma', 1e-10, 1),
+        'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+        'eval_metric': 'mlogloss',
+        'random_state': 0,
+        'n_jobs': -1
+    }
+    
+    model = XGBClassifier(**params)
+
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def catboost(trial, scoring, cv):
+    params = {
+        'iterations': trial.suggest_int('iterations', 100, 1000),
+        'depth': trial.suggest_int('depth', 1, 10),
+        'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.1),
+        'l2_leaf_reg': trial.suggest_float('l2_leaf_reg', 0.1, 10.0),
+        'random_seed': 0,
+        'loss_function': 'MultiClass',
+        'eval_metric': 'MultiClass',
+        'verbose': False
+    }
+    
+    model = CatBoostClassifier(**params)
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def rf(trial, scoring, cv):
+    max_depth = trial.suggest_int('max_depth', 1, 100)
+    n_estimators = trial.suggest_int('n_estimators', 10, 500)
+    min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 100)
+    min_samples_split = trial.suggest_int('min_samples_split', 2, 100)
+    random_state = 0
+          
+    model = RandomForestClassifier(
+        max_depth=max_depth,
+        n_estimators=n_estimators,
+        min_samples_leaf=min_samples_leaf,
+        min_samples_split=min_samples_split,
+        random_state=random_state
+    )
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def gbc(trial, scoring, cv):
+    #tol = trial.suggest_loguniform('tol', 1e-8, 10.0)
+    max_depth = trial.suggest_int('max_depth', 1, 50)
+    learning_rate = trial.suggest_loguniform('learning_rate', .001, 1)
+    n_estimators = trial.suggest_int('n_estimators', 10, 500)
+    min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 100)
+    max_leaf_nodes = trial.suggest_int('max_leaf_nodes', 2, 100)
+    random_state = 0
+          
+    model = GradientBoostingClassifier(
+        #tol=tol,
+        max_depth=max_depth,
+        learning_rate=learning_rate,
+        n_estimators=n_estimators,
+        min_samples_leaf=min_samples_leaf,
+        max_leaf_nodes=max_leaf_nodes,
+        random_state=random_state
+    )
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def et(trial, scoring, cv):
+    max_depth = trial.suggest_int('max_depth', 1, 100)
+    n_estimators = trial.suggest_int('n_estimators', 10, 500)
+    min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 100)
+    min_samples_split = trial.suggest_int('min_samples_split', 2, 100)
+    max_features = trial.suggest_categorical('max_features', ['auto', 'sqrt', 'log2', None])
+    random_state = 0
+
+          
+    model = ExtraTreesClassifier(
+        max_depth=max_depth,
+        n_estimators=n_estimators,
+        min_samples_leaf=min_samples_leaf,
+        min_samples_split=min_samples_split,
+        max_features=max_features,
+        random_state=random_state
+    )
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def lr(trial, scoring, cv):
+    C = trial.suggest_loguniform('C', 0.001, 1000)
+    solver = trial.suggest_categorical('solver', ['newton-cg', 'liblinear', 'sag', 'saga'])
+    
+    if solver=='sag' or solver=='newton-cg':
+        penalty = 'l2'
+        multi_class = trial.suggest_categorical('multi_class', ['ovr', 'multinomial'])
+    elif solver=='liblinear':
+        multi_class = 'ovr'
+        penalty = trial.suggest_categorical('penalty', ['l1', 'l2'])
+        if penalty==1:
+            l1_ratio = trial.suggest_float(0, 1)
+    elif solver=='saga':
+        multi_class = trial.suggest_categorical('multi_class', ['ovr', 'multinomial'])
+        penalty = trial.suggest_categorical('penalty', ['l1', 'l2'])
+        if penalty==1:
+            l1_ratio = trial.suggest_float(0, 1)
+          
+    model = LogisticRegression(
+        C=C,
+        penalty = penalty,
+        solver=solver,
+        multi_class=multi_class
+    )
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+    
+def ridge(trial, scoring, cv):
+    alpha = trial.suggest_int('alpha', 0, 1000)
+    tol = trial.suggest_loguniform('tol', 1e-8, 10.0)
+        
+    model = RidgeClassifier(
+        alpha=alpha,
+        tol=tol
+    )
+    calibrated_model = CalibratedClassifierCV(model, method='sigmoid')
+    
+    calibrated_model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def lda(trial, scoring, cv):
+    solver = trial.suggest_categorical('solver', ['lsqr', 'eigen'])
+    tol = trial.suggest_loguniform('tol', 1e-8, 10.0)
+          
+    model = LinearDiscriminantAnalysis(
+        solver=solver,
+        tol=tol
+    )
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def nb(trial, scoring, cv):
+    var_smoothing  = trial.suggest_loguniform('var_smoothing', 1e-10, 1e-3)
+          
+    model = GaussianNB(
+        var_smoothing=var_smoothing
+    )
+    
+    model.fit(X, y)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def ada(trial, scoring, cv):
+    learning_rate = trial.suggest_loguniform('learning_rate', .001, 1)
+    n_estimators = trial.suggest_int('n_estimators', 10, 500)
+          
+    model = AdaBoostClassifier(
+        learning_rate=learning_rate,
+        n_estimators=n_estimators
+    )
+    
+    model.fit(train_final, num_target)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
+
+def knn(trial, scoring, cv):
+    n_neighbors = trial.suggest_int('n_neighbors', 2, 100)
+          
+    model = KNeighborsClassifier(
+        n_neighbors=n_neighbors
+    )
+    
+    model.fit(X, target)
+    
+    cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+    
+    return np.mean(cv_scores)
---
+++ regressor_utils.py
@@ -0,0 +1,185 @@
+def gbr(trial, scoring, cv):
+    n_estimators = trial.suggest_int('n_estimators', 50, 1000)
+    learning_rate = trial.suggest_uniform('learning_rate', 0.001, 0.1)
+    max_depth = trial.suggest_int('max_depth', 2, 10)
+    min_samples_split = trial.suggest_int('min_samples_split', 2, 10)
+    min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 10)
+    
+    model = GradientBoostingRegressor(n_estimators=n_estimators,
+                                       learning_rate=learning_rate,
+                                       max_depth=max_depth,
+                                       min_samples_split=min_samples_split,
+                                       min_samples_leaf=min_samples_leaf)
+    
+    model.fit(X, y)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def lgbm(trial, scoring, cv):
+    params = {
+        'objective': 'regression',
+        'metric': 'mse',
+        'verbosity': -1,
+        'boosting_type': 'gbdt',
+        'num_leaves': trial.suggest_int('num_leaves', 2, 256),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 1),
+        'subsample': trial.suggest_float('subsample', 0.1, 1),
+        'feature_fraction': trial.suggest_float('feature_fraction', 0.1, 1),
+        'bagging_fraction': trial.suggest_float('bagging_fraction', 0.1, 1),
+        'bagging_freq': trial.suggest_int('bagging_freq', 1, 10),
+        'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
+        'max_depth': trial.suggest_int('max_depth', 2, 30),
+        'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
+        'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
+        'min_gain_to_split': trial.suggest_loguniform('min_gain_to_split', 0.1, 5),
+        'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 1, 100),
+        'random_state': 42
+    }
+    
+    model = LGBMRegressor(**params)
+    
+    model.fit(X, y)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def xgb(trial, scoring, cv):
+    n_estimators = trial.suggest_int('n_estimators', 50, 1000)
+    learning_rate = trial.suggest_uniform('learning_rate', 0.001, 0.1)
+    max_depth = trial.suggest_int('max_depth', 2, 10)
+    subsample = trial.suggest_uniform('subsample', 0.1, 1.0)
+    colsample_bytree = trial.suggest_uniform('colsample_bytree', 0.1, 1.0)
+    
+    model = XGBRegressor(n_estimators=n_estimators,
+                             learning_rate=learning_rate,
+                             max_depth=max_depth,
+                             subsample=subsample,
+                             colsample_bytree=colsample_bytree)
+    
+    model.fit(X, y)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def rfr(trial, scoring, cv):
+    n_estimators = trial.suggest_int('n_estimators', 50, 1000, step=100)
+    max_depth = trial.suggest_int('max_depth', 2, 30, step=2)
+    min_samples_split = trial.suggest_int('min_samples_split', 2, 10)
+    min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 10)
+    max_features = trial.suggest_uniform('max_features', 0.1, 1.0)
+    
+    model = RandomForestRegressor(n_estimators=n_estimators,
+                                   max_depth=max_depth,
+                                   min_samples_split=min_samples_split,
+                                   min_samples_leaf=min_samples_leaf,
+                                   max_features=max_features)
+
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def lr(trial, scoring, cv):
+    fit_intercept = trial.suggest_categorical('fit_intercept', [True, False])
+    
+    model = LinearRegression(fit_intercept=fit_intercept)
+    
+    model.fit(X, y)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def ridge(trial, scoring, cv):
+    alpha = trial.suggest_int('alpha', 0, 1000)
+    tol = trial.suggest_loguniform('tol', 1e-8, 10.0)
+        
+    model = Ridge(
+        alpha=alpha,
+        tol=tol
+    )
+    
+    model.fit(train_final, log_target)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def br(trial, scoring, cv):
+    n_iter = trial.suggest_int('n_iter', 50, 600)
+    tol = trial.suggest_loguniform('tol', 1e-8, 10.0)
+    alpha_1 = trial.suggest_loguniform('alpha_1', 1e-8, 10.0)
+    alpha_2 = trial.suggest_loguniform('alpha_2', 1e-8, 10.0)
+    lambda_1 = trial.suggest_loguniform('lambda_1', 1e-8, 10.0)
+    lambda_2 = trial.suggest_loguniform('lambda_2', 1e-8, 10.0)
+    
+    model = BayesianRidge(
+        n_iter=n_iter,
+        tol=tol,
+        alpha_1=alpha_1,
+        alpha_2=alpha_2,
+        lambda_1=lambda_1,
+        lambda_2=lambda_2
+    )
+    
+    model.fit(train_final, log_target)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
+
+def omp(trial, scoring, cv):
+    tol = trial.suggest_loguniform('tol', 1e-8, 10.0)
+    
+    model = OrthogonalMatchingPursuit(
+        tol=tol
+    )
+    
+    model.fit(train_final, log_target)
+    
+    if scoring=='neg_mean_squared_error':
+        np.exp(np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+    elif scoring=='neg_mean_absolute_error':
+        cv_scores = (-cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv))
+    else:
+        cv_scores = cross_val_score(model, X, y, scoring=scoring, cv=cv)
+        
+    return np.mean(cv_scores)
